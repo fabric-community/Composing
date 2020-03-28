6c2b977b0af3f175bd02d42f52134d96b3c2d5d3
JaredR6 <jarodrig@andrew.cmu.edu>
2020-03-27 20:16:49 -0400

    Initial refactor

--- src/main/java/io/teamblue/composing/Composing.java
+++ src/main/java/io/teamblue/composing/Composing.java
@@ -1,12 +1,11 @@
-package io.github.cottonmc.starter;
+package io.teamblue.composing;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class FabricStarter implements ModInitializer {
-	public static final String MODID = "starter";
-
+public class Composing implements ModInitializer {
+	public static final String MODID = "composing";
 	public static final Logger logger = LogManager.getLogger();
 
 	@Override
