b25f5d6c9f887185a318216c907f18c1ab3eec17
JaredR6 <jarodrig@andrew.cmu.edu>
2020-03-28 14:37:24 -0400

    Added item group

--- src/main/java/io/teamblue/composing/Composing.java
+++ src/main/java/io/teamblue/composing/Composing.java
@@ -4,12 +4,17 @@ import net.fabricmc.api.ModInitializer;
 
 import io.teamblue.composing.item.ComposingItems;
 
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Composing implements ModInitializer {
 	public static final String MODID = "composing";
 	public static final Logger logger = LogManager.getLogger();
+	public static final ItemGroup ITEMGROUP = FabricItemGroupBuilder.build(new Identifier(Composing.MODID, "tab"), () -> new ItemStack(ComposingItems.BLESSING_STONE));
 
 	@Override
 	public void onInitialize() {
--- src/main/java/io/teamblue/composing/item/ComposingItems.java
+++ src/main/java/io/teamblue/composing/item/ComposingItems.java
@@ -1,6 +1,9 @@
 package io.teamblue.composing.item;
 
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -8,25 +11,26 @@ import io.teamblue.composing.Composing;
 import static io.teamblue.composing.api.CrystalElement.*;
 
 public class ComposingItems {
-	public static final CrystalItem SMALL_FIRE_CRYSTAL = register(new CrystalItem(new Item.Settings(), FIRE, 0), "small_fire_crystal");
-	public static final CrystalItem MEDIUM_FIRE_CRYSTAL = register(new CrystalItem(new Item.Settings(), FIRE, 1), "medium_fire_crystal");
-	public static final CrystalItem LARGE_FIRE_CRYSTAL = register(new CrystalItem(new Item.Settings(), FIRE, 2), "large_fire_crystal");
+
+	public static final CrystalItem SMALL_FIRE_CRYSTAL = register(new CrystalItem(new Item.Settings().group(Composing.ITEMGROUP), FIRE, 0), "small_fire_crystal");
+	public static final CrystalItem MEDIUM_FIRE_CRYSTAL = register(new CrystalItem(new Item.Settings().group(Composing.ITEMGROUP), FIRE, 1), "medium_fire_crystal");
+	public static final CrystalItem LARGE_FIRE_CRYSTAL = register(new CrystalItem(new Item.Settings().group(Composing.ITEMGROUP), FIRE, 2), "large_fire_crystal");
 	
-	public static final CrystalItem SMALL_EARTH_CRYSTAL = register(new CrystalItem(new Item.Settings(), EARTH, 0), "small_earth_crystal");
-	public static final CrystalItem MEDIUM_EARTH_CRYSTAL = register(new CrystalItem(new Item.Settings(), EARTH, 1), "medium_earth_crystal");
-	public static final CrystalItem LARGE_EARTH_CRYSTAL = register(new CrystalItem(new Item.Settings(), EARTH, 2), "large_earth_crystal");
+	public static final CrystalItem SMALL_EARTH_CRYSTAL = register(new CrystalItem(new Item.Settings().group(Composing.ITEMGROUP), EARTH, 0), "small_earth_crystal");
+	public static final CrystalItem MEDIUM_EARTH_CRYSTAL = register(new CrystalItem(new Item.Settings().group(Composing.ITEMGROUP), EARTH, 1), "medium_earth_crystal");
+	public static final CrystalItem LARGE_EARTH_CRYSTAL = register(new CrystalItem(new Item.Settings().group(Composing.ITEMGROUP), EARTH, 2), "large_earth_crystal");
 	
-	public static final CrystalItem SMALL_WIND_CRYSTAL = register(new CrystalItem(new Item.Settings(), WIND, 0), "small_wind_crystal");
-	public static final CrystalItem MEDIUM_WIND_CRYSTAL = register(new CrystalItem(new Item.Settings(), WIND, 1), "medium_wind_crystal");
-	public static final CrystalItem LARGE_WIND_CRYSTAL = register(new CrystalItem(new Item.Settings(), WIND, 2), "large_wind_crystal");
+	public static final CrystalItem SMALL_WIND_CRYSTAL = register(new CrystalItem(new Item.Settings().group(Composing.ITEMGROUP), WIND, 0), "small_wind_crystal");
+	public static final CrystalItem MEDIUM_WIND_CRYSTAL = register(new CrystalItem(new Item.Settings().group(Composing.ITEMGROUP), WIND, 1), "medium_wind_crystal");
+	public static final CrystalItem LARGE_WIND_CRYSTAL = register(new CrystalItem(new Item.Settings().group(Composing.ITEMGROUP), WIND, 2), "large_wind_crystal");
 	
-	public static final CrystalItem SMALL_WATER_CRYSTAL = register(new CrystalItem(new Item.Settings(), WATER, 0), "small_water_crystal");
-	public static final CrystalItem MEDIUM_WATER_CRYSTAL = register(new CrystalItem(new Item.Settings(), WATER, 1), "medium_water_crystal");
-	public static final CrystalItem LARGE_WATER_CRYSTAL = register(new CrystalItem(new Item.Settings(), WATER, 2), "large_water_crystal");
+	public static final CrystalItem SMALL_WATER_CRYSTAL = register(new CrystalItem(new Item.Settings().group(Composing.ITEMGROUP), WATER, 0), "small_water_crystal");
+	public static final CrystalItem MEDIUM_WATER_CRYSTAL = register(new CrystalItem(new Item.Settings().group(Composing.ITEMGROUP), WATER, 1), "medium_water_crystal");
+	public static final CrystalItem LARGE_WATER_CRYSTAL = register(new CrystalItem(new Item.Settings().group(Composing.ITEMGROUP), WATER, 2), "large_water_crystal");
 	
-	public static final StoneItem BLESSING_STONE = register(new StoneItem(new Item.Settings(), 0), "blessing_stone");
-	public static final StoneItem SOUL_STONE = register(new StoneItem(new Item.Settings(), 0), "soul_stone");
-	public static final StoneItem HOLY_STONE = register(new StoneItem(new Item.Settings(), 0), "holy_stone");
+	public static final StoneItem BLESSING_STONE = register(new StoneItem(new Item.Settings().group(Composing.ITEMGROUP), 0), "blessing_stone");
+	public static final StoneItem SOUL_STONE = register(new StoneItem(new Item.Settings().group(Composing.ITEMGROUP), 0), "soul_stone");
+	public static final StoneItem HOLY_STONE = register(new StoneItem(new Item.Settings().group(Composing.ITEMGROUP), 0), "holy_stone");
 	
 	private static <T extends Item> T register(T item, String id) {
 		return Registry.register(Registry.ITEM, new Identifier(Composing.MODID, id), item);
