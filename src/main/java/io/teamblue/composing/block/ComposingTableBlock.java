b99db5eec67957d976a3dde37357d2c910c5f443
Martmists <martmists@gmail.com>
2020-04-03 22:26:24 +0200

    Change block parent

--- src/main/java/io/teamblue/composing/block/ComposingTableBlock.java
+++ src/main/java/io/teamblue/composing/block/ComposingTableBlock.java
@@ -22,7 +22,7 @@ import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
-public class ComposingTableBlock extends BlockWithEntity {
+public class ComposingTableBlock extends Block implements BlockEntityProvider {
     public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
 
     protected ComposingTableBlock() {
@@ -38,9 +38,9 @@ public class ComposingTableBlock extends BlockWithEntity {
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (hit.getSide() == Direction.UP) {
-            double[] slot1Area;
-            double[] slot2Area;
-            double[] slot3Area;
+            double[] slot1Area = new double[] {};
+            double[] slot2Area = new double[] {};
+            double[] slot3Area = new double[] {};
             double[] centerArea = new double[] { 0, 0, 1, 1 };
 
             Vec3d hitPos = hit.getPos().subtract(new Vec3d(hit.getBlockPos()));
